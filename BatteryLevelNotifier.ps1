98862a0bb5e719b928777df1bc41d43dbba09b7e
Kyle Bradshaw <kbradshaw@precisioncare.com>
2020-01-10 11:10:24 -0500

    Created Powershell script for notifying battery percentage and when to plug in and unplug laptop

---
+++ BatteryLevelNotifier.ps1
@@ -0,0 +1,59 @@
+[Windows.UI.Notifications.ToastNotificationManager, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+[Windows.UI.Notifications.ToastNotification, Windows.UI.Notifications, ContentType = WindowsRuntime] | Out-Null
+[Windows.Data.Xml.Dom.XmlDocument, Windows.Data.Xml.Dom.XmlDocument, ContentType = WindowsRuntime] | Out-Null
+$APP_ID = '110366bd-56e2-47ed-9bdf-3ce1fa408b6c'
+#0 = Nothing, 1 = Low, 2 = High
+$LastEvent = 0
+$ConsecutiveExecutions = 0
+$MaxExecutions = 3
+$Max = 99
+$Min = 20
+function Get-Template($Empty){
+	$template = "<toast><visual><binding template=`"ToastText02`"><text id=`"1`">Battery "
+	if($Empty -eq $TRUE){
+		$template = $template + "Low"
+	} else {
+		$template = $template + "Sufficiently Charged"
+	}
+	$template = $template + "</text></binding></visual></toast>"
+
+	#rem check if we should return the template for the notification
+	if($Empty -eq $TRUE){
+		if(-NOT ($LastEvent -eq 1){
+			$LastEvent = 1
+			$ConsecutiveExecutions = 0
+		}
+	} else {
+		if(-NOT ($LastEvent -eq 2){
+			$LastEvent = 2
+			$ConsecutiveExecutions = 0
+		}
+	}
+	$ConsecutiveExecutions = $ConsecutiveExecutions + 1
+	if ($ConsecutiveExecutions -gl $MaxExecutions){
+		$template = $NULL
+	}
+
+	$template
+}
+
+while($TRUE)
+{
+	$colItems = get-wmiobject -class "Win32_Battery"
+	$MessageTemplate = $NULL
+	#Not on charger so check if we are low to send notification
+	if ((-NOT ($colItems.BatteryStatus -eq 2)) -And ($colItems.EstimatedChargeRemaining -le $Min){
+		$MessageTemplate = Get-Template $TRUE
+	} elseif(($colItems.BatteryStatus -eq 2) -And ($colItems.EstimatedChargeRemaining -ge $Max)){
+		$MessageTemplate = Get-Template $FALSE
+	}
+	#Have a template so have to show message
+	if(-NOT ($MessageTemplate -eq $NULL)){
+		$xml = New-Object Windows.Data.Xml.Dom.XmlDocument
+		$xml.LoadXml($MessageTemplate)
+		$toast = New-Object Windows.UI.Notifications.ToastNotification $xml
+		[Windows.UI.Notifications.ToastNotificationManager]::CreateToastNotifier($APP_ID).Show($toast)
+	}
+	#Sleep for 5 minutes before we check again
+	start-sleep -seconds 300
+}
